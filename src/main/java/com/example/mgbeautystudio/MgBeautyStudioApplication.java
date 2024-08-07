b86974f0275322d1343e6794b3242bccd16cc747
Dominik <dominiksobczyk21@gmail.com>
2024-08-07 10:30:41 +0200

    first-commit

---
+++ src/main/java/com/example/mgbeautystudio/MgBeautyStudioApplication.java
@@ -0,0 +1,14 @@
+
+package com.example.mgbeautystudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MgBeautyStudioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MgBeautyStudioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mgbeautystudio/MgBeautyStudioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mgbeautystudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MgBeautyStudioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
