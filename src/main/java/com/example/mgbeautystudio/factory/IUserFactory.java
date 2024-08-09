f58a3d2fd511a28dd8926eaaf31c84e3d95c4e8a
Dominik <dominiksobczyk21@gmail.com>
2024-08-09 09:55:44 +0200

    simple factory classes implementation

--- src/main/java/com/example/mgbeautystudio/factory/IUserFactory.java
+++
@@ -1,8 +0,0 @@
-package com.example.mgbeautystudio.factory;
-
-import com.example.mgbeautystudio.model.User;
-import com.example.mgbeautystudio.request.RegistrationRequest;
-
-public interface IUserFactory {
-    public User createUser(RegistrationRequest registrationRequest);
-}
